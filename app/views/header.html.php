da060a541c7f0fa31ecc8009ea809ae6cd9b2c49
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2016-07-27 23:58:30 +0200

    Modify login page layout and navigation bar presentation

--- app/views/header.html.php
+++ app/views/header.html.php
@@ -22,14 +22,16 @@
     <img class="logo" src="http://www.insschool.fr/wp-content/uploads/2012/08/logo-site-noir1.jpg" alt="Logo">
 
     <nav>
-      <?php link_home() ?>
-      <?php link_table('lesson') ?>
-      <?php link_table('teacher') ?>
-      <?php link_table('room') ?>
-      <?php link_table('goody') ?>
-      <?php link_table('order') ?>
-      <?php link_table('member') ?>
-      <?php link_table('pre_registration') ?>
+      <ul>
+        <li><?php link_home() ?></li>
+        <li><?php link_table('lesson') ?></li>
+        <li><?php link_table('teacher') ?></li>
+        <li><?php link_table('room') ?></li>
+        <li><?php link_table('goody') ?></li>
+        <li><?php link_table('order') ?></li>
+        <li><?php link_table('member') ?></li>
+	<li><?php link_table('pre_registration') ?></li>
+      </ul>
     </nav>
   </div>
 </header>
--- app/views/login.html.php
+++ app/views/login.html.php
@@ -13,12 +13,9 @@
 </head>
 <body>
 
-<header class="login-header">
-  <img class="logo" src="http://www.insschool.fr/wp-content/uploads/2012/08/logo-site-noir1.jpg" alt="Logo">
-  <h1>Espace de gestion INS School</h1>
-</header>
+<div class="login-container">
+  <img src="http://www.insschool.fr/wp-content/uploads/2012/08/logo-site-noir1.jpg" alt="Logo">
 
-<div class="login-form">
   <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
     <p>Nom d'utilisateur<br>
     <input type="text" name="username" required="required"><br>
