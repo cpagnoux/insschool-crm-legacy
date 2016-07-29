6900dcb888fe9c179910ef7a89dc74acb2aea9e7
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2016-07-29 05:08:58 +0200

    style.css: Modify margin-bottom for .container, add margin-top to .footer-container

--- app/src/libentity.php
+++ app/src/libentity.php
@@ -423,7 +423,7 @@ function check_dependencies($link, $table, $id)
 }
 
 /*
- * Helper function to manage goodies stock according to orders
+ * Helper function for managing goodies stock according to orders
  */
 function update_goody_stock($link, $goody_id, $difference)
 {
