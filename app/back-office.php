73ff3c2229dba80cd2715e83933d701a7a54fdd8
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2016-09-01 16:23:31 +0200

    Extract session init from front controller to a function

--- app/back-office.php
+++ app/back-office.php
@@ -6,6 +6,7 @@
 set_include_path(__DIR__);
 
 require_once 'src/connection.php';
+require_once 'src/session.php';
 require_once 'src/login.php';
 require_once 'src/util.php';
 require_once 'src/mail.php';
@@ -17,27 +18,7 @@ require_once 'src/pre-registration.php';
 $_GET = sql_escape_strings($_GET);
 $_POST = sql_escape_strings($_POST);
 
-session_start();
-
-if (isset($_POST['member_filter']))
-	$_SESSION['member_filter'] = $_POST['member_filter'];
-if (isset($_POST['order_filter_by_member']))
-	$_SESSION['order_filter_by_member'] = $_POST['order_filter_by_member'];
-if (isset($_POST['order_filter']))
-	$_SESSION['order_filter'] = $_POST['order_filter'];
-if (isset($_POST['goody_sorting']))
-	$_SESSION['goody_sorting'] = $_POST['goody_sorting'];
-if (isset($_POST['lesson_sorting']))
-	$_SESSION['lesson_sorting'] = $_POST['lesson_sorting'];
-if (isset($_POST['order_sorting']))
-	$_SESSION['order_sorting'] = $_POST['order_sorting'];
-if (isset($_POST['person_sorting']))
-	$_SESSION['person_sorting'] = $_POST['person_sorting'];
-if (isset($_POST['room_sorting']))
-	$_SESSION['room_sorting'] = $_POST['room_sorting'];
-
-if (isset($_POST['limit']))
-	$_SESSION['limit'] = $_POST['limit'];
+init_session();
 
 $action = '';
 
---
+++ app/src/session.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Copyright (C) 2016 Christophe Pagnoux-Vieuxfort
+ */
+
+function init_session()
+{
+	session_start();
+
+	if (isset($_POST['member_filter']))
+		$_SESSION['member_filter'] = $_POST['member_filter'];
+	if (isset($_POST['order_filter_by_member']))
+		$_SESSION['order_filter_by_member'] =
+				$_POST['order_filter_by_member'];
+	if (isset($_POST['order_filter']))
+		$_SESSION['order_filter'] = $_POST['order_filter'];
+	if (isset($_POST['goody_sorting']))
+		$_SESSION['goody_sorting'] = $_POST['goody_sorting'];
+	if (isset($_POST['lesson_sorting']))
+		$_SESSION['lesson_sorting'] = $_POST['lesson_sorting'];
+	if (isset($_POST['order_sorting']))
+		$_SESSION['order_sorting'] = $_POST['order_sorting'];
+	if (isset($_POST['person_sorting']))
+		$_SESSION['person_sorting'] = $_POST['person_sorting'];
+	if (isset($_POST['room_sorting']))
+		$_SESSION['room_sorting'] = $_POST['room_sorting'];
+
+	if (isset($_POST['limit']))
+		$_SESSION['limit'] = $_POST['limit'];
+}
+?>
