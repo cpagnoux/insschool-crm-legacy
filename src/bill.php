f26741104c601e9f834ad2d2adb6b9b07fb44d6d
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2016-12-02 21:33:31 +0100

    bill.php: Add constants for font and font size

--- src/bill.php
+++ src/bill.php
@@ -5,6 +5,11 @@
 
 require_once 'vendor/fpdf181/fpdf.php';
 
+define('FONT', 'Arial');
+define('FONT_SIZE_SMALL', 8);
+define('FONT_SIZE_NORMAL', 10);
+define('FONT_SIZE_BIG', 12);
+
 class Bill extends FPDF
 {
 	function Header()
@@ -13,9 +18,9 @@ class Bill extends FPDF
 			     50);
 		$this->Ln(5);
 
-		$this->SetFont('Arial', 'B', 10);
+		$this->SetFont(FONT, 'B', FONT_SIZE_NORMAL);
 		$this->Cell(50, 5, 'Association INS School', 0, 2);
-		$this->SetFont('Arial', '', 10);
+		$this->SetFont(FONT, '', FONT_SIZE_NORMAL);
 		$this->Cell(50, 5, '29 rue Jules Verne', 0, 2);
 		$this->Cell(50, 5, '63100 Clermont-Ferrand', 0, 2);
 		$this->Cell(50, 5, 'Siret : 528 602 410 00011', 0, 2);
@@ -26,7 +31,7 @@ class Bill extends FPDF
 	function Footer()
 	{
 		$this->setY(-20);
-		$this->SetFont('Arial', '', 8);
+		$this->SetFont(FONT, '', FONT_SIZE_SMALL);
 		$this->Cell(0, 5, 'Culturelle - SIRET : 52860241000011', 'T', 1,
 			    'C');
 		$this->Cell(0, 5, $this->PageNo() . '/{nb}', 0, 0, 'R');
@@ -35,15 +40,15 @@ class Bill extends FPDF
 	function PrintBillInfo($bill_num, $date, $member_code)
 	{
 		$this->SetXY(100, 10);
-		$this->SetFont('Arial', 'B', 12);
+		$this->SetFont(FONT, 'B', FONT_SIZE_BIG);
 		$this->Cell(90, 10, 'Facture', 1, 2, 'C');
-		$this->SetFont('Arial', 'B', 10);
+		$this->SetFont(FONT, 'B', FONT_SIZE_NORMAL);
 		$this->Cell(30, 5, utf8_decode('N° facture'), 1, 0, 'C', true);
 		$this->Cell(30, 5, 'Date', 1, 0, 'C', true);
 		$this->Cell(30, 5, 'Code client', 1, 2, 'C', true);
 
 		$this->SetX(100);
-		$this->SetFont('Arial', '', 10);
+		$this->SetFont(FONT, '', FONT_SIZE_NORMAL);
 		$this->Cell(30, 5, $bill_num, 1, 0, 'C');
 		$this->Cell(30, 5, $date, 1, 0, 'C');
 		$this->Cell(30, 5, $member_code, 1, 0, 'C');
@@ -53,10 +58,10 @@ class Bill extends FPDF
 	function PrintMemberInfo($data)
 	{
 		$this->SetX(100);
-		$this->SetFont('Arial', 'B', 10);
+		$this->SetFont(FONT, 'B', FONT_SIZE_NORMAL);
 		$this->Cell(90, 10, $data['last_name'] . ' ' .
 				   $data['first_name'], 'LRT', 2);
-		$this->SetFont('Arial', '', 10);
+		$this->SetFont(FONT, '', FONT_SIZE_NORMAL);
 		$this->Cell(90, 10, $data['address'], 'LR', 2);
 		$this->Cell(90, 10, $data['postal_code'] . ' ' . $data['city'],
 			    'LRB');
@@ -66,13 +71,13 @@ class Bill extends FPDF
 	function PrintBillDetail($data)
 	{
 		$this->SetY(80);
-		$this->SetFont('Arial', 'B', 10);
+		$this->SetFont(FONT, 'B', FONT_SIZE_NORMAL);
 		$this->Cell(100, 5, 'Description', 1, 0, 'C');
 		$this->Cell(30, 5, utf8_decode('Quantité'), 1, 0, 'C');
 		$this->Cell(30, 5, 'Prix HT', 1, 0, 'C');
 		$this->Cell(30, 5, 'Total HT', 1, 1, 'C');
 
-		$this->SetFont('Arial', '', 10);
+		$this->SetFont(FONT, '', FONT_SIZE_NORMAL);
 
 		foreach ($data as $row) {
 			$this->Cell(100, 10, $row['description'], 1);
@@ -99,7 +104,7 @@ class Bill extends FPDF
 		$to_be_paid = sprintf('%.2f', $total_ttc - $total_paid);
 
 		$this->SetY(230);
-		$this->SetFont('Arial', 'B', 10);
+		$this->SetFont(FONT, 'B', FONT_SIZE_NORMAL);
 		$this->Cell(20, 5, '% TVA', 1, 0, 'C', true);
 		$this->Cell(20, 5, 'Base', 1, 0, 'C', true);
 		$this->Cell(30, 5, 'Montant TVA', 1, 0, 'C', true);
@@ -109,7 +114,7 @@ class Bill extends FPDF
 		$this->Cell(30, 5, utf8_decode('Déjà réglé TTC'), 1, 1, 'C',
 			    true);
 
-		$this->SetFont('Arial', '', 10);
+		$this->SetFont(FONT, '', FONT_SIZE_NORMAL);
 		$this->Cell(20, 5, $tva_shown, 1, 0, 'R');
 		$this->Cell(20, 5, $total, 1, 0, 'R');
 		$this->Cell(30, 5, $tva_amount, 1, 0, 'R');
@@ -118,10 +123,10 @@ class Bill extends FPDF
 		$this->Cell(30, 5, $total_ttc, 1, 0, 'R');
 		$this->Cell(30, 5, $total_paid, 1, 2, 'R');
 
-		$this->SetFont('Arial', 'B', 10);
+		$this->SetFont(FONT, 'B', FONT_SIZE_NORMAL);
 		$this->Cell(30, 5, utf8_decode('Net à payer'), 1, 2, 'C', true);
 
-		$this->SetFont('Arial', '', 10);
+		$this->SetFont(FONT, '', FONT_SIZE_NORMAL);
 		$this->Cell(30, 5, $to_be_paid, 1, 0, 'R');
 	}
 }
