3910178ae90cc3b203e80c267e6aeba79426044c
Christophe Pagnoux-Vieuxfort <christophe.pagnouxv@gmail.com>
2017-02-20 00:18:02 +0100

    Update .htaccess files

--- generate_package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp -r public gestion
-cp .htaccess gestion
-zip -r insschool-extranet.zip app vendor gestion
-rm -r gestion
--- generate_package_testing.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp -r public public_html
-zip -r insschool-extranet.zip app vendor public_html
-rm -r public_html
